3f8916b9032fb114f7e6552f8337fde1f4bbcaaa
Charlie Knight <williamchknight@gmail.com>
2020-11-07 01:18:14 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,14 +13,18 @@ end
 
 def literal_aoh
   [{:name => "Don Gately", occupation: "Live-in Staff"},
-    :name =>"Joelle van Dyne", occupation: "Radio Personality"  
+    {:name =>"Joelle van Dyne", occupation: "Radio Personality"},
+  { :name =>  "Pat Monteseian", occupation: "Staff" },
+  { :name =>  "Kate Gompert", occupation: "None" },
+  { :name => "Bruce Green", occupation: "Fan of Mildred"}
    ]
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
-  # Update the AoH data at row and key to have the value of new_value
-  # Return the updated AoH
+ aoh[row][key] = new_value
+ aoh
 end
